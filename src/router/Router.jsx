f852ad80f8907505a7027e4178586b27ae91f4c3
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-03 19:05:52 +0900

    get state from privious page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,6 +1,5 @@
 import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
-import { Page2 } from "../Page2";
 import { page1Routes } from "./Page1Routes";
 import { page2Routes } from "./Page2Routes";
 
